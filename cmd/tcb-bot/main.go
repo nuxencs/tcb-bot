ddd61951e150c486e67e468c324a5c42a0051151
nuxen <47067662+nuxencs@users.noreply.github.com>
2023-08-03 01:30:20 +0200

    chore(notifications): moved notifications to own func

--- cmd/tcb-bot/main.go
+++ cmd/tcb-bot/main.go
@@ -311,18 +311,8 @@ func processHTMLElement(e *colly.HTMLElement, discord *discordgo.Session) {
 
 				// Send notification to Discord
 				log.Debug().Str("chapter", mangaTitle).Msg("Sending notification to discord")
-				_, err := discord.ChannelMessageSendEmbed(config.DiscordChannelID, &discordgo.MessageEmbed{
-					Title:       manga,
-					Description: fmt.Sprintf("Chapter %s: %s\n", chapter, chapterTitle),
-					URL:         websiteURL + mangaLink,
-					Footer: &discordgo.MessageEmbedFooter{
-						Text: "Released at " + formattedTime,
-					},
-					Color: 3447003,
-				})
-				if err != nil {
-					log.Fatal().Str("chapter", mangaTitle).Err(err).Msg("Error sending Discord notification")
-				}
+				sendDiscordNotification(manga, fmt.Sprintf("Chapter %s: %s\n", chapter, chapterTitle),
+					websiteURL+mangaLink, "Released at "+formattedTime, 3447003)
 				saveCollectedChapters()
 				log.Info().Str("chapter", mangaTitle).Msg("Notification sent")
 			} else {
@@ -330,6 +320,21 @@ func processHTMLElement(e *colly.HTMLElement, discord *discordgo.Session) {
 			}
 			break
 		}
+	}
+}
+
+func sendDiscordNotification(title string, description string, url string, footer string, color int) {
+	_, err := discord.ChannelMessageSendEmbed(config.DiscordChannelID, &discordgo.MessageEmbed{
+		Title:       title,
+		Description: description,
+		URL:         url,
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: footer,
+		},
+		Color: color,
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error sending Discord notification")
 	}
 }
 
@@ -394,6 +399,8 @@ func main() {
 		err := collector.Visit(websiteURL)
 		if err != nil {
 			log.Error().Err(err).Msg("Error visiting website, trying again in the next cycle")
+			sendDiscordNotification("Error visiting website", fmt.Sprintf("%s", err), "",
+				"", 10038562)
 		}
 	}
 }
