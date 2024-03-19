902b1e1fa6bc704ae6b314077c0145f4a9cfb7b4
nuxen <theonenuxen@proton.me>
2024-03-19 22:09:19 +0100

    refactor(validation): make regex compilation global var

--- internal/utils/validation.go
+++ internal/utils/validation.go
@@ -2,12 +2,15 @@ package utils
 
 import "regexp"
 
+var (
+	releaseTitleRegex = regexp.MustCompile(`^(.+?) Chapter (\d+(\.\d+)?)$`)
+	releaseLinkRegex  = regexp.MustCompile(`^/chapters/\d+/[a-z0-9-]+-chapter-\d+.*$`)
+)
+
 func ValidateReleaseTitle(releaseTitle string) bool {
-	re := regexp.MustCompile(`^(.+?) Chapter (\d+(\.\d+)?)$`)
-	return re.MatchString(releaseTitle)
+	return releaseTitleRegex.MatchString(releaseTitle)
 }
 
 func ValidateReleaseLink(releaseLink string) bool {
-	re := regexp.MustCompile(`^/chapters/\d+/[a-z0-9-]+-chapter-\d+.*$`)
-	return re.MatchString(releaseLink)
+	return releaseLinkRegex.MatchString(releaseLink)
 }
